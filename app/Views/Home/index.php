bfd02bba9a96ccebe1ed45475d18e34893050ce0
aymane <elmainiaymane03@gmail.com>
2023-12-31 16:40:30 +0100

    PAR-42   i create the header and footer

--- app/Views/Home/index.php
+++ app/Views/Home/index.php
@@ -1,1 +1,5 @@
-hello <?= $data['name']?>
+<?php require_once APP_ROOT . '/Views/components/header.php';?>
+
+dfjsldfjlj <?= $data['name']?>
+
+<?php require_once APP_ROOT . '/Views/components/footer.php';?>
